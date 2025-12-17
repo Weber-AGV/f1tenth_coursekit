0b05bd3bb81d031ce72899643173e9f4ddf83d6e
hadziklabs <scott@hadziklabs.com>
2025-12-17 00:17:04 -0700

    removed sphynx-tabs

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ needs_sphinx = '1.3'
 
 # Sphinx extension module names and templates location
 sys.path.append(os.path.abspath('extensions'))
-extensions = ['sphinx_tabs.tabs', 'sphinx.ext.imgmath', 'sphinx.ext.todo']
+extensions = [ 'sphinx.ext.imgmath', 'sphinx.ext.todo']
 
 templates_path = ['_templates']
 
